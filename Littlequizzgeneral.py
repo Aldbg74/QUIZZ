70805b7957c509c59aa8b63468ad4df7d6bf3ed4
Alexis <63512507+Aldbg74@users.noreply.github.com>
2023-01-18 11:54:49 +0100

    Add the first quizz
    Little Quiz General.

---
+++ Littlequizzgeneral.py
@@ -0,0 +1,11 @@
+# Little quizz
+
+# Simple quiz en utilisant les dictionnaires
+def main() :
+    # Dictionnaire des questions
+    questions = {
+        "Quel est le nom du président de la République Française ?" : "Emmanuel Macron",
+        "Quel est le nom du président de la République du Sénégal ?" : "Macky Sall",
+        "Quel est le nom du président de la République de la Côte d'Ivoire ?" : "Alassane Ouattara",
+        "Quel est le nom du président de la République du Mali ?" : "Ibrahim B"
+    }
