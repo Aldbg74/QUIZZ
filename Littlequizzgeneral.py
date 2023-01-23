6d4f51e5a4b171b85c71f3b17945cc09f49d1eb1
Alexis <63512507+Aldbg74@users.noreply.github.com>
2023-01-23 20:21:28 +0100

    Add files via upload

--- Littlequizzgeneral.py
+++ Littlequizzgeneral.py
@@ -1,16 +1,43 @@
+#
+#
 # Little quizz
+#
+#
+#Alexis.D 
+#2023
 
-# Simple quiz en utilisant les dictionnaires
+
+#Import library
+from random import shuffle
+
+# Quiz questions 
+# Define the questions and her answers
+
 def questions() :
     questions = {
-        "Who is in 2023 the French President ?" : "Emmanuel Macron",
-        "Who is the US president in 2023 ?" : "Joe Biden",
-        "What is the capital of France ?" : "Paris",
-        "What is the capital of the US ?" : "Washington",
-        "What is the capital of the UK ?" : "London",
-        "Who write the song 'Hybrid Theory' ?" : "Linkin Park",
+        "Who is in 2023 the French President ?"
+        "Who is the US president in 2023 ?"
+        "What is the capital of France ?"
+        "What is the capital of the US ?"
+        "What is the capital of the UK ?"
+        "Who write the song 'Hybrid Theory' ?"
+        "Who is the singer of the song 'Numb' ?"
+        "Who is nothing nowere ?"
+        "What is the biggest country of the world ?"
     }
 
+def answer() :
+    answer = {
+        "Emmanuel Macron" : "Who is in 2023 the French President ?",
+        "Joe Biden" : "Who is the US president in 2023 ?",
+        "Paris" : "What is the capital of France ?",
+        "Washington" : "What is the capital of the US ?",
+        "London" : "What is the capital of the UK ?",
+        "Linkin Park" : "Who write the song 'Hybrid Theory' ?",
+        "Chester Bennington" : "Who is the singer of the song 'Numb' ?",
+        "Nothing" : "Who is nothing nowere ?",
+        "Russia" :  "What is the biggest country of the world ?"
+    }
 #Launch the quiz by starting a dialog with the user
 def start_quiz() :
     print("Welcome to the quiz !")
@@ -27,12 +54,143 @@ def start_quiz() :
             print("The correct answer is : " + answer)
 
 # Nothing to see here
+
+
+
+
+
+
+
+
+
 # Really, nothing to see here
+
+
+
+
+
+
+
+
+
+
 # I'm not even kidding
-# There's nothing to see here
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # I'm serious
-# There's nothing to see here
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # I'm not kidding
+
+
+
+
+
+
+
+
+
+
+
+
+
+# What do you think ?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Finding a secret ?
+
+
+
+
+
+
+
+
+
+
+# Finding a task list of the futur of the  quiz ??
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ha ha ha ha ha ha
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # To be created/done
 # See why this shit isn't working
